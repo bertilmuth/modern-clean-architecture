1abdfa7bea8dc6614544458d441d2eb6c0817bb9
Bertil Muth <bertilmuth@yahoo.de>
2021-08-07 18:16:55 +0200

    Remove EmptyResponse class

--- samples/todolist/src/main/java/com/example/todolist/behavior/response/EmptyResponse.java
+++
@@ -1,9 +0,0 @@
-package com.example.todolist.behavior.response;
-
-/**
- * An empty response that is returned in case the request is just consumed without an explicit response.
- * @author b_muth
- *
- */
-public class EmptyResponse {
-}
